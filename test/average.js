1552f7f85eebeff71f4d0c413fd8e1349a8744c9
an-0611 <39644763+an-0611@users.noreply.github.com>
2019-08-20 14:46:06 +0800

    create test folder & create mocha average.js

---
+++ test/average.js
@@ -0,0 +1,18 @@
+const should = require('should')
+const average = require('../lib/average')
+describe('#average', () => {
+  // 測試算出來的平均是不是 2.5
+  it('should return the average of array', done => {
+    var avg = average([1, 2, 3, 4])
+    avg.should.equal(2.5)
+    done()
+  })
+  // 測試有沒有回傳 NaN
+  it('should return NaN when array is empty', done => {
+    var avg = average([])
+    isNaN(avg).should.be.true
+    done()
+  })
+})
+
+// 求出陣列中的平均值，如果陣列是空的則傳回 NaN
