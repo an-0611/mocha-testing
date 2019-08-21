e6ed465b2837bad6c4979755f9a97d61367162ec
An <tt50914@settour.com.uw>
2019-08-21 09:07:09 +0800

    update index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+// module.exports.indexOf = require('lib/indexOf');
+module.exports.average = require('lib/average');
+module.exports.max = require('lib/max');
+module.exports.min = require('lib/min');
